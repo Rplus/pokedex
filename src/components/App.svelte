a4fb29ff2500ea30e40115ec7fe4be7811fde06f
Rplus <rplus.tw@gmail.com>
2020-02-17 07:15:41 +0800

    basic structure from lukeed/svelte-demo

---
+++ src/components/App.svelte
@@ -0,0 +1,66 @@
+<Nav {active} />
+
+<main>
+	<svelte:component this={Route} {params} />
+</main>
+
+<script>
+	import Navaid from 'navaid';
+	import About from '../routes/About.svelte';
+	import Home from '../routes/Home.svelte';
+
+	import { onDestroy } from 'svelte';
+	import Nav from './Nav.svelte';
+
+	let Route, params, active;
+	let uri = location.pathname;
+	$: active = uri.split('/')[1] || 'home';
+
+	function draw(m, obj) {
+		params = obj || {};
+		if (m.preload) {
+			m.preload({ params }).then(() => {
+				Route = m.default;
+				window.scrollTo(0, 0);
+			});
+		} else {
+			Route = m.default;
+			window.scrollTo(0, 0);
+		}
+	}
+
+	function track(obj) {
+		uri = obj.state || obj.uri || location.pathname;
+		if (window.ga) ga.send('pageview', { dp:uri });
+	}
+
+	addEventListener('replacestate', track);
+	addEventListener('pushstate', track);
+	addEventListener('popstate', track);
+
+	const router = Navaid('/', () => console.log(404))
+		// .on('/', () => import('../routes/Home.svelte').then(draw))
+		// .on('/about', () => import('../routes/About.svelte').then(draw))
+		.on('/', () => {
+			Route = Home;
+		})
+		.on('/about', () => {
+			Route = About;
+		})
+		// .on('/blog', () => import('../routes/Blog.svelte').then(draw))
+		// .on('/blog/:postid', obj => import('../routes/Article.svelte').then(m => draw(m, obj)))
+		.listen();
+
+	onDestroy(router.unlisten);
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
