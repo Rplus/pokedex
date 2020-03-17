8699f4e7ddd2e7cb1456d81d3cd001fd38dc6380
Rplus <rplus.tw@gmail.com>
2020-03-18 06:20:19 +0800

    replace base href when deploy

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,10 @@ set -e
 # navigate into the build output directory
 cd './public'
 
+# replace base href
+sed -i -e 's/href="\/"/href="\/pokedex\/"/g' ./index.html
+sed -i -e 's/href="\/"/href="\/pokedex\/"/g' ./404.html
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
